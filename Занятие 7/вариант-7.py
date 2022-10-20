a1f09dbcba525830aa9809b230dcc100f056f855
DedenevAI <104195790+DedenevAI@users.noreply.github.com>
2022-10-21 00:39:37 +0300

    Delete Занятие 7 directory

--- Занятие 7/вариант-7.py
+++
@@ -1,9 +0,0 @@
-# 7 Вариант
-def fun(x):
-    n = len(x)
-    x = s[:n // 2].replace('п', '*') + s[n // 2:]
-    return x
-
-s = "пппппппыфыпппфывфвыпппвыфвфпппппппavottytezhedrugiebukvi"
-print(fun(s))
-
