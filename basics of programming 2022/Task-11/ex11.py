2cdd2caca163a8ed64a9c1f9e8ba1e527ecdd5de
Dedenev <dedenevai3263@yandex.ru>
2022-11-30 01:43:59 +0300

    Change path

--- basics of programming 2022/Task-11/ex11.py
+++ basics of programming 2022/Task-11/ex11.py
@@ -7,7 +7,7 @@ import requests
 from tkinter import*
 #@this function implements a button click, returns the result in the form of information from JSON git
 def buttonAction():
-    with open("D:\\GitProject\\DedenevAI\\DedenevAI\\basics of programming 2022\\Ex11\\file_for_write_data","w") as file:
+    with open("D:\\GitProject\\DedenevAI\\DedenevAI\\basics of programming 2022\\Task-11\\file_for_write_data","w") as file:
         user = txtField.get()
         url = f"https://api.github.com/users/{user}"
         userInfo = requests.get(url).json()
